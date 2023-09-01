a80693728327e55b10700e3d956203718a838bfd
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-09-01 17:37:01 -0400

    write pseudocode

--- home-page/app.js
+++ home-page/app.js
@@ -18,5 +18,41 @@
 
 
 
+// 1) Define the required variables used to track the state of the game
+// Create an array that keeps track of the flipped cards
+// Create an array that keeps track of matching pairs
+// Create a variable that counts total moves and increment it by 1 every time the player moves
 
+
+// 2) Store cached element references
+// HTML elements that represent the cards
+
+
+// 3) Initialize a game by calling a function to render the game
+
+
+// 4) Define the required constants
+// Create an array of cards
+// Create an array of winning combinations
+
+
+// 5) Add event listeners to cards
+
+
+// 6) Create a function to handle a player clicking a card 
+
+
+// 7) Create a function to update the state of the board
+
+
+// 8) Create a function to switch the turn of the player
+
+
+// 9) Create a function to check if there’s a tie
+
+
+// 10) Create a function to check if there’s a winner
+
+
+// 11) Create a function to reset the game
 
